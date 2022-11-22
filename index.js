68dc3dff88ad370d4cdce91ceb830200b57bde3b
callum <callumpholt@gmail.com>
2022-11-23 05:22:37 +1100

    commit tree aa8217390d74592336f7996ea025e3112a3f9445

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+// let bestCustomer
+// console.log(bestCustomer);
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'john';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'mat';
+    return leastFavoriteCustomer;
+}
+
+changeLeastFavoriteCustomer();
+console.log(changeLeastFavoriteCustomer());
